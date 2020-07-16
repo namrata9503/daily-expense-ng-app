d987aea07f26fae19e43b608bad9c4ef5fae1d1b
namrata9503 <nam9503@gmail.com>
2020-07-16 22:56:28 +0200

    update total budget

--- src/app/budget-item-list/budget-item-list.component.ts
+++ src/app/budget-item-list/budget-item-list.component.ts
@@ -8,9 +8,13 @@ import { EditItemModalComponent } from "../edit-item-modal/edit-item-modal.compo
   templateUrl: "./budget-item-list.component.html",
   styleUrls: ["./budget-item-list.component.scss"]
 })
+
+
 export class BudgetItemListComponent implements OnInit {
+
   @Input() budgetItems: BudgetItem[];
   @Output() delete: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
+  @Output() update: EventEmitter<UpdateEvent> = new EventEmitter<UpdateEvent>();
   constructor(public dialog: MatDialog) {}
 
   ngOnInit() {}
@@ -28,8 +32,17 @@ export class BudgetItemListComponent implements OnInit {
       if (result) {
         //result is updated item
         //replace the item with updated item of form
-        this.budgetItems[this.budgetItems.indexOf(item)] = result;
+       // this.budgetItems[this.budgetItems.indexOf(item)] = result;
+
+       this.update.emit({
+         old: item,
+         new: result
+       })
       }
     });
   }
+}
+export interface UpdateEvent{
+  old: BudgetItem,
+  new: BudgetItem
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { BudgetItem } from 'src/shared/models/budget-items.model';
+import { Component, OnInit } from "@angular/core";
+import { BudgetItem } from "src/shared/models/budget-items.model";
+import { UpdateEvent } from '../budget-item-list/budget-item-list.component';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-budgetItems: BudgetItem[]= new Array<BudgetItem>();
-  constructor() { }
+  total: number = 0;
 
-  ngOnInit() {
+  budgetItems: BudgetItem[] = new Array<BudgetItem>();
+  constructor() {}
+
+  ngOnInit() {}
+
+  addItem(newItem: BudgetItem) {
+    this.budgetItems.push(newItem);
+    this.total += newItem.amount;
   }
 
-  addItem(newItem: BudgetItem){
-      this.budgetItems.push(newItem);
-  }
-
-  deleteItem(item : BudgetItem){
-    let index= this.budgetItems.indexOf(item);
+  deleteItem(item: BudgetItem) {
+    let index = this.budgetItems.indexOf(item);
     this.budgetItems.splice(index, 1);
+    this.total -= item.amount;
+
+  }
+
+  updateItem(updateItem: UpdateEvent){
+
+       //result is updated item
+        //replace the item with updated item of form
+       this.budgetItems[this.budgetItems.indexOf(updateItem.old)] = updateItem.new;
+
+       //update total budget
+       this.total -= updateItem.old.amount;
+       this.total += updateItem.new.amount;
 
   }
 }
